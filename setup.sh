e2f0c4d26405826f38f734c3464593cab4460e0a
geeks-dev <info@geeks-dev.com>
2015-07-20 21:41:02 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -2,4 +2,7 @@
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.vimbackup ~/.vimbackup
 
+git -C ~/dotfiles submodule init
+git -C ~/dotfiles submodule update
